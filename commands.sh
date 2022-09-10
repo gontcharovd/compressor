623c35d2c66cd8719d747d71d04a5274241643ad
Denis Gontcharov <denis@gontcharov.be>
2022-09-10 14:33:10 +0200

    stuck on linked template

--- commands.sh
+++ commands.sh
@@ -2,20 +2,21 @@
 
 # parameters
 resource="key_vault"
+templateFile="./azure-deploy-compressor-dev.json"
+# templateFile=="./${resource}/template.json"
+# devParameterFile="./${resource}/parameters.json"
 resourceGroup="compressor"
 deploymentName="compressor-dev"
 
 # az group create \
-#   --name myResourceGroup \
+#   --name $resourceGroup \
 #   --location "West Europe"
 
-templateFile="./${resource}/template.json"
-devParameterFile="./${resource}/parameters.json"
 az deployment group create \
   --name $deploymentName \
   --resource-group $resourceGroup \
-  --template-file $templateFile \
-  --parameters $devParameterFile
+  --template-file $templateFile #\
+  # --parameters $devParameterFile
 
 # resourcelist=$(az deployment group  show --resource-group $resourceGroup --name $deploymentName --query "properties.outputResources[].id" -o tsv)
 # for resource in $resourcelist; do az resource delete --ids $resource; done
