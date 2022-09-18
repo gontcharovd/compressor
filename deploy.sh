e84e99f1a916e801c916e3d0617a3bddb0da3d25
Denis Gontcharov <denis@gontcharov.be>
2022-09-18 13:31:21 +0200

    add destroy script and delete arm templates

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@ location=westeurope
 resourceGroupName=compressor
 
 az account set --name compressor
-
 az group create --location $location --resource-group $resourceGroupName
-
 az deployment group create \
     --resource-group $resourceGroupName \
     --template-file main-template/main.bicep
