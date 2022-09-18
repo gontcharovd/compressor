f548ffa43d52af49520d5e1f50079cc7dc5bcc9a
Denis Gontcharov <denis@gontcharov.be>
2022-09-18 15:15:57 +0200

    add bash scripts to .gitignore

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-location=westeurope
-resourceGroupName=compressor
-
-az account set --name compressor
-# az group create --location $location --resource-group $resourceGroupName
-az deployment group create \
-    --resource-group $resourceGroupName \
-    --template-file main-template/main.bicep
--- destroy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-location=westeurope
-resourceGroupName=compressor
-
-az account set --name compressor
-az group delete  --resource-group $resourceGroupName --yes
-az keyvault purge --name keyVault7cwkv6diblxjy --location $location 
